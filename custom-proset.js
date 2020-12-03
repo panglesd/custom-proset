ca577047d07232dae99bc882a449b58d325f9bb5
Paul-Elliot Anglès d'Auriac <peada@free.fr>
2020-12-04 00:31:29 +0100

    printing proof of concept (with file...)

---
+++ custom-proset.js
@@ -0,0 +1,115 @@
+let updateItem = function(i) {
+    let image = document.querySelector("#img-item-"+i);
+    let reader  = new FileReader();
+    let file = document.querySelector('#file'+i).files[0];
+    reader.addEventListener("load", function () {
+	image.src = reader.result;
+    });
+    if(file) {
+	reader.readAsDataURL(file);
+    }
+};
+
+for(let i=0; i<6;i++) {
+    document.querySelector("#file"+i).addEventListener("change", () => {updateItem(i);});
+}
+
+let getCoordInElem = (ev) => {
+    return [ev.pageX - ev.target.offsetLeft, ev.pageY - ev.target.offsetTop];
+};
+let getCoordOfElem = (elem) => {
+    return [elem.offsetLeft, elem.offsetTop];
+};
+
+let scaleDistance = (x,y) => {
+    return Math.sqrt((x[0]-y[0])*(x[0]-y[0])+(x[1]-y[1])*(x[1]-y[1]));
+};
+
+////////////////////////////::
+// Scale Tool
+////////////////////////////::
+
+let scaleTool = new Object();
+scaleTool.mouseUp = (ev) => {scaleTool.originClick = undefined;};
+scaleTool.mouseDown = (ev) => {
+    scaleTool.originClick = [ev.pageX, ev.pageY];
+    scaleTool.originalWidth = ev.target.width;
+};
+scaleTool.mouseClick = (ev) => {console.log(ev);};
+scaleTool.mouseMove = (ev) => {
+    if(typeof (scaleTool.originClick) != "undefined") {
+	ev.target.width = scaleTool.originalWidth + (ev.pageX - scaleTool.originClick[0]);
+    }
+};
+
+////////////////////////////::
+// Move Tool
+////////////////////////////::
+
+let moveTool = new Object();
+moveTool.mouseUp = (ev) => {moveTool.originClick = undefined;};
+moveTool.mouseDown = (ev) => {
+    moveTool.originClick = [ev.pageX, ev.pageY];
+    moveTool.originPosition = getCoordOfElem(ev.target);
+};
+moveTool.mouseClick = (ev) => {console.log(ev);};
+moveTool.mouseMove = (ev) => {
+    if(typeof (moveTool.originClick) != "undefined") {
+	console.log(ev.target);
+	ev.target.style.left=(moveTool.originPosition[0]+(ev.pageX-moveTool.originClick[0]))+"px";
+	ev.target.style.top=(moveTool.originPosition[1]+(ev.pageY-moveTool.originClick[1]))+"px";
+	
+	moveTool.originClick = [ev.pageX, ev.pageY];
+	moveTool.originPosition = getCoordOfElem(ev.target);
+	
+	// console.log([ev.pageX - ev.target.offsetLeft, ev.pageY - ev.target.offsetTop]);
+    }
+};
+
+let noTool = new Object();
+noTool.mouseUp = () => {};
+noTool.mouseDown = () => {};
+noTool.mouseClick = (ev) => {};
+noTool.mouseMove = () => {};
+
+let currentTool;
+
+
+let setCurrentTool = (tool) => {
+    currentTool = tool;
+};
+setCurrentTool(noTool);
+
+document.querySelector("#scale-tool").addEventListener("click", () => {setCurrentTool(scaleTool);});
+document.querySelector("#move-tool").addEventListener("click", () => {setCurrentTool(moveTool);});
+
+document.querySelectorAll(".item").forEach((item) =>{
+    item.addEventListener("mouseup", (ev) => {currentTool.mouseUp(ev);});
+    item.addEventListener("mousedown", (ev) => {currentTool.mouseDown(ev);});
+    item.addEventListener("click", (ev) => {currentTool.mouseClick(ev);});
+    item.addEventListener("mousemove", (ev) => {currentTool.mouseMove(ev);});
+});
+
+
+
+////////////////////////////::
+// Printing
+////////////////////////////::
+
+let print = () => {
+    let cardWidth = document.querySelector(".card").offsetWidth;
+    let scale = 64/cardWidth;
+
+    let docu = new jspdf.jsPDF("p", "mm", "a4");
+
+    docu.rect(0,0,64,89);
+    document.querySelectorAll(".item").forEach((item,i) => {
+	if(item.src == "")
+	    return;
+	let [x,y] = getCoordOfElem(item);
+	console.log((item.src, x*scale, y*scale, item.width*scale, item.height*scale));
+	docu.addImage(item.src, x*scale, y*scale, item.width*scale, item.height*scale);
+    });
+    docu.save("my.pdf");
+
+};
