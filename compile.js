83394bbeb674f8930373761f42cf0cefe0c384e7
Paul-Elliot Anglès d'Auriac <peada@free.fr>
2020-12-23 16:00:08 +0100

    adding commandline arguments

--- compile.js
+++ compile.js
@@ -4,9 +4,9 @@ console.log(print);
 
 const fs = require('fs');
 
-let rawJSON = fs.readFileSync('./custom-proset.txt');
+let rawJSON = fs.readFileSync('./'+process.argv[2]);
 let json = JSON.parse(rawJSON);
 
 print.printFromJSON(json, jspdf).then((docu) => {
-    docu.save("blibli.pdf");
+    docu.save(process.argv[3]);
 });
